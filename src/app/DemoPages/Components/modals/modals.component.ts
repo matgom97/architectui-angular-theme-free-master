dd7c13ac241c2a776bcf5506151bbe938480bf41
Mateo Gomez <teosio97@gmail.com>
2026-08-04 21:03:19 -0500

    feat: version2

--- src/app/DemoPages/Components/modals/modals.component.ts
+++ src/app/DemoPages/Components/modals/modals.component.ts
@@ -1,4 +1,4 @@
-import {Component, ChangeDetectionStrategy, EventEmitter, Output} from '@angular/core';
+import {Component, ChangeDetectionStrategy, EventEmitter, Output, Input} from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
@@ -11,6 +11,16 @@ import { HttpClient } from '@angular/common/http';
 export class ModalsComponent {
 
   @Output() cerrar = new EventEmitter<void>();
+  //Emitir cuando se guarda
+  @Output() guardarCredito = new EventEmitter<any>();
+  @Input() isCreation: boolean = false;
+  @Input() credito: any = null; // Variable para recibir el crédito a editar
+  @Input() isView: boolean = false; // Variable para determinar si es vista o edición
+
+
+  // Con ternario
+  title = ""; 
+
   
 
   form!: FormGroup;
@@ -29,6 +39,24 @@ export class ModalsComponent {
       tasaInteres: ['', Validators.required],
       valorCredito: ['', Validators.required],
     });
+    this.title = this.isCreation ? 'Crear Crédito' : 'Editar Crédito';
+
+    // Mostrar los datos del crédito en el formulario si es edición
+    if (!this.isCreation && this.credito || this.isView) {
+      this.form.patchValue({
+        cedula: this.credito.cedula,
+        nombres: this.credito.nombres,
+        apellidos: this.credito.apellidos,
+        plazo: this.credito.plazo,
+        tasaInteres: this.credito.tasaInteres,
+        valorCredito: this.credito.valorCredito
+      });
+    }
+
+    console.log(this.title);
+    console.log(this.credito);
+    
+    
   }
 
 
@@ -38,19 +66,39 @@ export class ModalsComponent {
 
   guardar(): void {
 
-    if (this.form.invalid) {
-      this.form.markAllAsTouched();
-      return;
-    }
-
-    this.http.post('http://localhost:8000/api/creditos', this.form.value)
-      .subscribe({
-        next: (resp) => console.log(resp),
-        error: (err) => console.error(err)
-      });
-
-    this.cerrarModal(); // Cierra el modal después de guardar
+  if (this.form.invalid) {
+    this.form.markAllAsTouched();
+    return;
   }
+
+  var json = {
+    cedula: this.form.value.cedula,
+    nombres: this.form.value.nombres,
+    apellidos: this.form.value.apellidos,
+    plazo: this.form.value.plazo,
+    tasaInteres: this.form.value.tasaInteres,
+    valorCredito: this.form.value.valorCredito,
+    id: this.credito ? this.credito.id : null
+}
+
+  const request = this.isCreation
+    ? this.http.post('http://localhost:8000/api/creditos', json)
+    : this.http.put('http://localhost:8000/api/creditos/update', json);
+
+  request.subscribe({
+    next: (resp) => {
+
+      // Avisar al padre cuando realmente terminó
+      this.guardarCredito.emit(resp);
+
+      // Cerrar modal
+      this.cerrar.emit();
+
+    },
+    error: (err) => console.error(err)
+  });
+
+}
 
 
 }
--- src/app/DemoPages/Dashboards/analytics/analytics.component.ts
+++ src/app/DemoPages/Dashboards/analytics/analytics.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
 import { faTh, faCheck, faTrash, faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import { ChartConfiguration } from 'chart.js';
 import { HttpClient } from '@angular/common/http';
@@ -10,15 +10,20 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
-  changeDetection: ChangeDetectionStrategy.Eager,
+  //changeDetection: ChangeDetectionStrategy.Default,
   standalone: false})
 export class AnalyticsComponent {
 
-  constructor(private http: HttpClient) {}
+  creditos: any[] = [];
+
+  constructor(private http: HttpClient, private cd: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.creditosList();
+    console.log(this.creditos);
   }
+
+  
 
   faTh = faTh;
   faCheck = faCheck;
@@ -192,9 +197,14 @@ export class AnalyticsComponent {
 
   showModals: boolean = false;
 
+  isCreation: boolean = false; // Variable para determinar si es creación o edición
+
   // Emitir estado del modal
   creditoModal() {
     this.showModals = true;
+    this.isCreation = true; // Establecer como creación al abrir el modal
+    this.selectedCredito = null; // Limpiar el crédito seleccionado al abrir el modal
+    this.isView = false;
     console.log('Modal abierto');
   }
 
@@ -202,7 +212,7 @@ export class AnalyticsComponent {
   this.showModals = false;
 }
 
-creditos: any[] = [];
+
 
 creditosList(): void {
   console.log("llegue aqui ");
@@ -213,28 +223,54 @@ creditosList(): void {
         console.log(resp);
         
         this.creditos = resp.data;
-
+        this.cd.detectChanges();
         console.log('Lista de créditos:', this.creditos);
       },
       error: (err) => console.error(err)
     });
+
+    console.log("sali de la peticion");
 }
 
 eliminarCredito(id: number): void {
-  this.http.delete(`http://localhost:8000/api/creditos/${id}`)
+  this.http.delete(`http://localhost:8000/api/creditos/destroy`, { params: { id: id } })
     .subscribe({
       next: () => {
         console.log(`Crédito con ID ${id} eliminado`);
         // Actualizar la lista de créditos después de eliminar
         this.creditosList();
+        this.cd.detectChanges();
+
       },
       error: (err) => console.error(err)
     });
 
   }
+  selectedCredito: any = null; // Variable para almacenar el crédito seleccionado para edición
 
   editarCredito(credito: any): void {
     // Aquí puedes implementar la lógica para editar un crédito
     console.log('Editar crédito:', credito);
+    this.isCreation = false; // Establecer como edición al abrir el modal
+    this.selectedCredito = credito;
+    this.showModals = true;
+    this.isView =   false; // Establecer como edición al abrir el modal 
   }
+
+  guardarCredito(credito: any): void {
+    // Aquí puedes implementar la lógica para guardar un crédito
+    console.log('Guardar crédito:', credito);
+    this.creditosList(); // Actualizar la lista de créditos después de guardar
+  }
+
+  isView: boolean = false; // Variable para determinar si es vista o edición
+  verCredito(credito: any): void {
+    // Aquí puedes implementar la lógica para ver un crédito
+    console.log('Ver crédito:', credito);
+    this.isCreation = false;
+    this.selectedCredito = credito;
+    this.showModals = true;
+    this.isView = true; // Establecer como vista al abrir el modal
+  }
+
 }
